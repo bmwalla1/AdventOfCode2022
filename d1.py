4ea0a4737a84038e8de72d768f2ddcc549bd8687
Brandon Wallace <brandonmwallace97@gmail.com>
2022-12-01 10:52:15 -0700

    d1

---
+++ d1.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def run():
+    inputfile = open('input.txt', 'r')
+    Lines = inputfile.readlines()
+
+    array = np.array([])
+
+    currentCount = 0
+    for line in Lines:
+        if line == "\n":
+            array = np.append(array, currentCount)
+            currentCount = 0
+        else:
+            currentCount += int(line)
+
+    array = np.sort(array)[::-1]
+    # part 1 answer
+    print(array[0])
+
+    # part 2 answer
+    print(array[0] + array[1] + array[2])
+
+
+if __name__ == '__main__':
+    run()
